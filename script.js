4aeb9423524a2f6326541949643e951ae9abdd4c
Ana Ortez <anaortez23@gmail.com>
2020-12-05 12:48:40 -0500

    fix html

--- script.js
+++ script.js
@@ -2,122 +2,140 @@ class Game{
     constructor() {
         this.canvas = undefined;
         this.ctx = undefined;
-        this.dx = 18;
-        this.dy = 18;
-        this.x = 240;
-        this.y = 0;
+        this.dx = 15;
+        this.dy = 15;
+        this.x = 400//235;
+        this.y = 40//5;
         this.WIDTH = 830;
         this.HEIGHT = 555;
         this.img = new Image();
-        this.collision = 0;
+        this.bunnyImg = new Image();
+        this.collision = false;
 
     }
     startMaze() {
-    let x = document.getElementById("myDIV");
-    x.style.display = "block";
-    // let button = document.getElementById("startMaze")
-    // console.log(button)
-    
-        // if (x.style.display === "none") {
-        // x.style.display = "block";
-        // } else {
-        // x.style.display = "none";
-        // }
+    document.getElementById("myDIV").style.display = "block";
+    // document.getElementById('mazeWin').style.display = 'none';
+    document.getElementById('welcomePage').style.display = 'none';
+    // this.img.this.removeBackground();
     }  
-    //Player rectangle
-    rect(x,y,w,h) {
-    this.ctx.beginPath();
-    this.ctx.rect(x,y,w,h);
-    this.ctx.closePath();
-    this.ctx.fill();
-    }
+    // //Player rectangle
+    // rect(x,y,w,h) {
+    // this.ctx.beginPath();
+    // this.ctx.rect(x,y,w,h);
+    // this.ctx.closePath();
+    // this.ctx.fill();
+    // }
     // Draw maze img
     clear() {
     this.ctx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
-    this.ctx.drawImage(this.img, -50, -50);
+    this.ctx.drawImage(this.img, -1, -1, this.WIDTH, this.HEIGHT);
     }
     
     //load img
     init() {
     this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
-    this.img.src = "images/maze (1).gif";
+    this.img.src = ("images/maze.png");
     const interval = setInterval(() => {
     this.draw()
+    this.win()
     }, 1000 / 60);
     }
     
     move() {
     document.addEventListener('keydown', event => {
+        // console.log(this);
     switch (event.code) {
         case 'ArrowUp':
         case 'KeyW': /* Up arrow was pressed */
-            if (this.y - this.dy > 0){
-            this.y -= this.dy;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.y += this.dy;
-            this.collision = 0;
-        }
-    }
-    break;
+            if (this.y - this.dy > 0 && !this.checkCollision(this.x + this.dx, this.y )){
+                this.y -= this.dy;
+                this.clear();
+                // this.checkCollision();
+            // if (this.collision == 1){
+            //     this.y += this.dy;
+            //     this.collision = 0;
+            // }
+            }
+        break;
         case 'ArrowDown':
         case 'KeyS':  /* Down arrow was pressed */
-            if (this.y + this.dy < this.HEIGHT ){
-            this.y += this.dy;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.y -= this.dy;
-            this.collision = 0;
-        }
-    }
-    break;
+            if (this.y + this.dy < this.HEIGHT && !this.checkCollision(this.x, this.y + this.dy)){
+                this.y += this.dy;
+                this.clear();
+                console.log("arrowDown")
+            //     this.checkCollision();
+            // if (this.collision == 1){
+            //     this.y -= this.dy;
+            //     this.collision = 0;
+            // }
+            }
+        break;
         case 'ArrowLeft':
         case 'KeyA':  /* Left arrow was pressed */
-            if (this.x - this.dx > 0){
+            if (this.x - this.dx > 0 && !this.checkCollision(this.x - this.dx, this.y)){
             this.x -= this.dx;
             this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.x += this.dx;
-            this.collision = 0;
-        }
+        //     this.checkCollision();
+        //     if (this.collision == 1){
+        //     this.x += this.dx;
+        //     this.collision = 0;
+        // }
         }
     break;
         case 'ArrowRight':
         case 'KeyD':  /* Right arrow was pressed */
-            if (this.x + this.dx < this.w) {
+            if (this.x + this.dx < this.WIDTH && !this.checkCollision(this.x + this.dx + 6, this.y)) {
             this.x += this.dx;
             this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.x -= this.dx;
-            this.collision = 0;
-        }
+        //     this.checkCollision();
+        //     if (this.collision == 1){
+        //     this.x -= this.dx;
+        //     this.collision = 0;
+        // }
         }
     break;
     }
     });
     }
     
-    checkCollision() {
-    let imgd = this.ctx.getImageData(this.x, this.y, 15, 15);
-    let pix = imgd.data;
-    // console.log(pix)
-    for (let i = 0; i < pix.length; i += 4) {
-    if (pix[i] == 0 ) {
-    this.collision = 1;
-    }
-    }
+    checkCollision(x, y) {
+        this.clear();
+        let imgd = this.ctx.getImageData(x, y, 20, 20);
+        console.log(x, y)
+        let pix = imgd.data;
+        // console.log(pix)
+        for (let i = 3; i < pix.length; i += 4) {
+            if (pix[i] > 0.7 ) { //&& pix[i+1] === 0 && pix[i+2] === 0) {
+            console.log(i);
+            return true;
+            // console.log("yes black")
+            }
+        }
+        return false;
     }
     
     draw() {
     this.clear();
-    this.ctx.fillStyle = "blue";
-    this.rect(this.x, this.y, 15, 15);
+    let carrotImg = new Image();
+    carrotImg.src = ("images/carrotPrice1.png");
+    this.ctx.drawImage(carrotImg, 390, 8, 50, 25);
+    this.bunnyImg.src = ("images/MazeBunny/frontRestBunny.png");
+    this.ctx.drawImage(this.bunnyImg, 6.2, 5.5, 90, 40, this.x, this.y, 150, 60);
+
     }
+
+    win(x, y) {
+        // console.log(this.x >= 390 , this.x < 410 , this.y <= 20 , this.y > 0)
+        if (this.x > 390 && this.x < 410 && this.y <= 20 && this.y > 0){
+        // console.log("now")
+        document.getElementById('myDIV').style.display = 'none';
+        document.getElementById('mazeWin').style.display = "block";
+    }
+    
+    }
+    
 }
 
 
@@ -137,6 +155,9 @@ class Game{
 
 const myGame = new Game();
 myGame.init();
+myGame.move();
+myGame.win();
+// myGame.win();
 // console.log(myGame.move)
 // window.addEventListener('keydown',myGame.move,true);
 
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// constructor
+// // constructor
 var canvas;
 var ctx;
 var dx = 18;
@@ -14,15 +14,8 @@ var collision = 0;
 function startMaze() {
     let x = document.getElementById("myDIV");
     x.style.display = "block";
-//     let button = document.getElementById("startMaze")
-// console.log(button)
-
-    // if (x.style.display === "none") {
-    // x.style.display = "block";
-    // } else {
-    // x.style.display = "none";
-    // }
 }
+
 //Player rectangle
 function rect(x,y,w,h) {
 ctx.beginPath();
@@ -36,154 +29,186 @@ ctx.fill();
 function clear() {
 ctx.clearRect(0, 0, WIDTH, HEIGHT);
 ctx.drawImage(img, -50, -50);
+// removeBackground(img);
 }
-//load img
+// //load img
 function init() {
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
-img.src = "images/maze (1).gif";
-return setInterval(draw, 10);
+img.src= ("images/maze (1).gif");
+
 }
 
-//movement
+// //movement
 function doKeyDown(evt){
 switch (evt.keyCode) {
-case 38:  /* Up arrow was pressed */
-if (y - dy > 0){
-y -= dy;
-clear();
-checkcollision();
-if (collision == 1){
-y += dy;
-collision = 0;
-}
-}
-
-break;
-case 40:  /* Down arrow was pressed */
-if (y + dy < HEIGHT ){
-y += dy;
-clear();
-checkcollision();
-if (collision == 1){
-y -= dy;
-collision = 0;
-}
-}
-
-break;
-case 37:  /* Left arrow was pressed */
-if (x - dx > 0){
-x -= dx;
-clear();
-checkcollision();
-if (collision == 1){
-x += dx;
-collision = 0;
-}
-}
-break;
-case 39:  /* Right arrow was pressed */
-if ((x + dx < WIDTH)){
-x += dx;
-clear();
-checkcollision();
-if (collision == 1){
-x -= dx;
-collision = 0;
-}
-}
-break;
-}
-}
+    case 38:  /* Up arrow was pressed */
+    if (y - dy > 0){
+        y -= dy;
+        clear();
+        checkcollision();
+        if (collision == 1){
+            y += dy;
+            collision = 0;
+            }
+            }
+            
+    break;
+    case 40:  /* Down arrow was pressed */
+    if (y + dy < HEIGHT ){
+        y += dy;
+        clear();
+        checkcollision();
+        if (collision == 1){
+            y -= dy;
+            collision = 0;
+            }
+            }
+            
+    break;
+    case 37:  /* Left arrow was pressed */
+    if (x - dx > 0){
+        x -= dx;
+        clear();
+        checkcollision();
+        if (collision == 1){
+            x += dx;
+            collision = 0;
+            }
+            }
+    break;
+    case 39:  /* Right arrow was pressed */
+    if ((x + dx < WIDTH)){
+        x += dx;
+        clear();
+        checkcollision();
+        if (collision == 1){
+            x -= dx;
+            collision = 0;
+    }
+    }
+    break;
+    }
+    }
 
 function checkcollision() {
 var imgd = ctx.getImageData(x, y, 15, 15);
 var pix = imgd.data;
 // console.log(pix)
 for (let i = 0; i < pix.length; i += 4) {
-if (pix[i] == 0 ) {
-collision = 1;
-}
-}
-}
-
+    if (pix[i] == 0 ) {
+        collision = 1;
+        }
+        }
+        }
+        
 function draw() {
-clear();
-ctx.fillStyle = "blue";
-rect(x, y, 15, 15);
-}
+    clear();
+    ctx.fillStyle = "blue";
+    rect(x, y, 15, 15);
+    }
+    
+    
+    // init();
+    // window.addEventListener('keydown',doKeyDown,true);
+    // console.log(checkcollision())
+    
+    
+    
+    
+    
+    
 
-
-init();
-window.addEventListener('keydown',doKeyDown,true);
-console.log(checkcollision())
-
-
-
-
-
-
-
-
-const myGame = new Game();
-myGame.startMaze()
-myGame.init();
+// const myGame = new Game();
+// myGame.startMaze()
+// myGame.init();
 // // console.log(init())
 
 
 
-move() {
-    document.addEventListener('keydown', event => {
-    switch (event.code) {
-        case 'ArrowUp':
-        case 'KeyW': /* Up arrow was pressed */
-            if (this.y - this.dy > 0){
-            this.y -= this.dy;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.y += this.dy;
-            this.collision = 0;
-        }
-    }
-    break;
-        case 'ArrowDown':
-        case 'KeyS':  /* Down arrow was pressed */
-            if (this.y + this.dy < this.height ){
-            this.y += this.dy;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.y -= this.dy;
-            this.collision = 0;
-        }
-    }
-    break;
-        case 'ArrowLeft':
-        case 'KeyA':  /* Left arrow was pressed */
-            if (this.x - this.dx > 0){
-            this.x -= this.dx;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.x += this.dx;
-            this.collision = 0;
-        }
-        }
-    break;
-        case 'ArrowRight':
-        case 'KeyD':  /* Right arrow was pressed */
-            if (this.x + this.dx < this.width) {
-            this.x += this.dx;
-            this.clear();
-            this.checkCollision();
-            if (this.collision == 1){
-            this.x -= this.dx;
-            this.collision = 0;
-        }
-        }
-    break;
-    }
-    });
-    }
+// move() {
+//     document.addEventListener('keydown', event => {
+//     switch (event.code) {
+//         case 'ArrowUp':
+//         case 'KeyW': /* Up arrow was pressed */
+//             if (this.y - this.dy > 0){
+//             this.y -= this.dy;
+//             this.clear();
+//             this.checkCollision();
+//             if (this.collision == 1){
+//             this.y += this.dy;
+//             this.collision = 0;
+//         }
+//     }
+//     break;
+//         case 'ArrowDown':
+//         case 'KeyS':  /* Down arrow was pressed */
+//             if (this.y + this.dy < this.height ){
+//             this.y += this.dy;
+//             this.clear();
+//             this.checkCollision();
+//             if (this.collision == 1){
+//             this.y -= this.dy;
+//             this.collision = 0;
+//         }
+//     }
+//     break;
+//         case 'ArrowLeft':
+//         case 'KeyA':  /* Left arrow was pressed */
+//             if (this.x - this.dx > 0){
+//             this.x -= this.dx;
+//             this.clear();
+//             this.checkCollision();
+//             if (this.collision == 1){
+//             this.x += this.dx;
+//             this.collision = 0;
+//         }
+//         }
+//     break;
+//         case 'ArrowRight':
+//         case 'KeyD':  /* Right arrow was pressed */
+//             if (this.x + this.dx < this.width) {
+//             this.x += this.dx;
+//             this.clear();
+//             this.checkCollision();
+//             if (this.collision == 1){
+//             this.x -= this.dx;
+//             this.collision = 0;
+//         }
+//         }
+//     break;
+//     }
+//     });
+//     }
+// function white2transparent(img)
+// {
+//     init();
+//     // var c = document.createElement('canvas');
+
+//     // var w = img.width, h = img.height;
+
+//     // c.width = w;
+//     // c.height = h;
+
+//     // var ctx = c.getContext('2d');
+
+//     ctx.drawImage(img, 0, 0, WIDTH, HEIGHT);
+//     var imageData = ctx.getImageData(0,0, WIDTH, HEIGHT);
+//     var pixel = imageData.data;
+
+//     var r=0, g=1, b=2,a=3;
+//     for (var p = 0; p<pixel.length; p+=4)
+//     {
+//       if (
+//           pixel[p+r] == 255 &&
+//           pixel[p+g] == 255 &&
+//           pixel[p+b] == 255) // if white then change alpha to 0
+//       {pixel[p+a] = 100;}
+//     }
+
+//     ctx.putImageData(imageData,0,0);
+
+//     return canvas.toDataURL('image/png');
+// }
+// var someimage = document.getElementById('imageid');
+// someimage.src = white2transparent(someimage);
